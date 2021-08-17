492c43c8243767f21278dd48688f8b8b4ae5821a
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-08-17 18:48:41 -0500

    add library swiper and complete view in mobil

--- views/user/js/animations.js
+++ views/user/js/animations.js
@@ -11,3 +11,16 @@ if (navToggle && navClose && navMenu) {
     navMenu.classList.remove("nav__visible");
   });
 }
+/* ========== recent products ========== */
+let offerSwiper = new Swiper(".offer", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".offer__btn-next",
+    prevEl: ".offer__btn-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
