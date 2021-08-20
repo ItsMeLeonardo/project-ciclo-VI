105161d78af8a3d682ec74333c59821b56e4cf90
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-08-19 19:13:05 -0500

    complete login and resolbe one problem of chat :,c

--- views/user/js/animations.js
+++ views/user/js/animations.js
@@ -42,20 +42,27 @@ if (chatShow && chatClose) {
     chatBox = document.querySelector('#chat-box');
   
   //create elements view message
-  const boxMessage = document.createElement("LI"),
+  const drawChatMessage = (message, type = 'request') => {
+    const boxMessage = document.createElement("LI"),
     messageContent = document.createElement("SPAN");
-
+    
     boxMessage.setAttribute("class", "chat__box");
     messageContent.setAttribute("class", "chat__message");
-
     boxMessage.appendChild(messageContent);
+
+    if (type !== 'request') type = 'response';
+    let typeMessageClass = 'chat__message--' + type;
+
+    boxMessage.classList.add(typeMessageClass);
+    messageContent.textContent = message;
+     
+    return boxMessage;
+  };
 
   sendBtn.addEventListener("click", () => {
     let message = chatWriter.value;
-    boxMessage.classList.add('chat__message--request');
-    messageContent.textContent = message;
+  
+    chatBox.appendChild(drawChatMessage(message));
     chatWriter.value = '';
-
-    chatBox.appendChild(boxMessage);
   });
 }
---
+++ views/user/js/form.js
@@ -0,0 +1,9 @@
+/* ========== Create element of errors ========== */
+const iconError = document.createElement('I'),
+  textError = document.createElement('SPAN');
+
+iconError.setAttribute('class', 'isax isax-information form__icon-alert');
+textError.setAttribute('class', 'form__text-alert');
+// <!-- <i class="isax isax-slash form__icon-alert"></i> -->
+// <!-- <i class="isax isax-warning-2 form__icon-alert"></i> -->
+// <!-- <i class="isax isax-verify form__icon-alert"></i> -->
